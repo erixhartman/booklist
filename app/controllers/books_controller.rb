e73cc68e7769aaff214a2a6063114dbbb16647a6
Eric Hartman <imerichartman@gmail.com>
2016-11-15 13:49:44 -0500

    Add respond_to for html

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,11 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    respond_to do |format|
+      format.html
+      
+      # format.json { render json: @resource }
+    end
   end
 
 end
